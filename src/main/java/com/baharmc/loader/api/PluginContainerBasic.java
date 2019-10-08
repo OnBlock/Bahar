86f6a2ae5d1483b383230737f79fc5bb21f15713
portlek <hsndmrts_@outlook.com>
2019-10-08 18:05:18 +0300

    name change

--- src/main/java/com/baharmc/loader/api/PluginContainerBasic.java
+++
@@ -1,8 +0,0 @@
-package com.baharmc.loader.api;
-
-import com.baharmc.loader.PluginContained;
-
-public class PluginContainerBasic implements PluginContained {
-
-    
-}
---
+++ src/main/java/com/baharmc/loader/contained/PluginContainerBasic.java
@@ -0,0 +1,25 @@
+package com.baharmc.loader.contained;
+
+import com.baharmc.loader.PluginContained;
+import com.baharmc.loader.metadata.PluginMetaData;
+
+import java.nio.file.Path;
+
+public class PluginContainerBasic implements PluginContained {
+
+    @Override
+    public PluginMetaData getMetaData() {
+        return null;
+    }
+
+    @Override
+    public Path getRootPath() {
+        return null;
+    }
+
+    @Override
+    public Path getPath(String file) {
+        return null;
+    }
+
+}
