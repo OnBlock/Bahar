d82ae213c8503ac1de85301be424ad6a99136b76
OnBlock-Dev <47112191+OnBlock@users.noreply.github.com>
2019-10-08 20:10:44 +0330

    Working on version thingies, added back the 'inStable' option in 'bahar.plugin.yml'

--- src/main/java/com/baharmc/loader/metadata/PluginMetaData.java
+++ src/main/java/com/baharmc/loader/metadata/PluginMetaData.java
@@ -1,12 +1,20 @@
 package com.baharmc.loader.metadata;
 
+import com.baharmc.loader.utils.semanticversion.Version;
+
 public class PluginMetaData  {
     /**
      * Required
      */
     private String id;
     private String name;
+    private Version version;
+    private boolean isStable;
+    private boolean isSnapshot;
 
+    /**
+     * Optional
+     */
 
 
 }
