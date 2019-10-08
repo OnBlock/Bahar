cef1e7a15441a45c1085b0d7e0fb475d6c218bb7
portlek <hsndmrts_@outlook.com>
2019-10-08 20:22:49 +0300

    remove schematic classes

--- src/main/java/com/baharmc/loader/utils/semanticversion/Version.java
+++
@@ -1,9 +0,0 @@
-package com.baharmc.loader.utils.semanticversion;
-
-public interface Version {
-    String getFriendlyString();
-
-    static Version parse(String string) throws VersionParsingException {
-        return VersionDeserializer.deserialize(string);
-    }
-}
