273bd94faf69f6acd0f754e147f67c788809bf51
portlek <hsndmrts_@outlook.com>
2019-10-08 20:36:00 +0300

    Update PluginContainerBasic.java

--- src/main/java/com/baharmc/loader/contained/PluginContainerBasic.java
+++ src/main/java/com/baharmc/loader/contained/PluginContainerBasic.java
@@ -1,11 +1,20 @@
 package com.baharmc.loader.contained;
 
 import com.baharmc.loader.PluginContained;
+import com.baharmc.loader.PluginMetaData;
 import com.baharmc.loader.metadata.PluginMetaDataBasic;
+import org.jetbrains.annotations.NotNull;
 
 import java.nio.file.Path;
 
 public class PluginContainerBasic implements PluginContained {
+
+    @NotNull
+    private final PluginMetaData pluginMetaData;
+
+    public PluginContainerBasic(@NotNull PluginMetaData pluginMetaData) {
+        this.pluginMetaData = pluginMetaData;
+    }
 
     @Override
     public PluginMetaDataBasic getMetaData() {
