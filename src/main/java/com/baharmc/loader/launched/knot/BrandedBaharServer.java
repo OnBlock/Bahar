afd86f51f3119225918fe5f9f91b6916a9a93746
OnBlock-Dev <47112191+OnBlock@users.noreply.github.com>
2019-10-09 00:45:25 +0330

    Added BrandedBaharServer

---
+++ src/main/java/com/baharmc/loader/launched/knot/BrandedBaharServer.java
@@ -0,0 +1,23 @@
+package com.baharmc.loader.launched.knot;
+
+import net.minecraft.server.MinecraftServer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(MinecraftServer.class)
+public class BrandedBaharServer {
+
+    @Inject(
+            method = "getServerModName", at = @At(
+            target = "Lnet/minecraft/server/MinecraftServer;getServerModName()Ljava/lang/String;",
+            value = "RETURN"
+    ))
+
+    private void Bahar$Brand(CallbackInfoReturnable<String> cir) {
+        cir.setReturnValue("Bahar");
+    }
+
+}
