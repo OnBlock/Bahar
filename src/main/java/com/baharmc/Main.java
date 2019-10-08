765c70de0494700c7efde49dcf715a2dcc294159
portlek <hsndmrts_@outlook.com>
2019-10-08 20:47:12 +0300

    launcher

--- src/main/java/com/baharmc/Main.java
+++ src/main/java/com/baharmc/Main.java
@@ -1,9 +1,11 @@
 package com.baharmc;
 
+import com.baharmc.loader.launched.LauncherBasic;
+
 public class Main {
 
     public static void main(String[] args) {
-        
+        new LauncherBasic(args).start();
     }
 
 }
--- src/main/java/com/baharmc/loader/launched/LauncherBasic.java
+++ src/main/java/com/baharmc/loader/launched/LauncherBasic.java
@@ -1,9 +1,19 @@
 package com.baharmc.loader.launched;
 
 import com.baharmc.loader.BaharLaunched;
+import org.jetbrains.annotations.NotNull;
 
 public class LauncherBasic implements BaharLaunched {
 
-    
+    @NotNull
+    private final String[] args;
+
+    public LauncherBasic(@NotNull String[] args) {
+        this.args = args;
+    }
+
+    public void start() {
+
+    }
 
 }
