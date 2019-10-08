aa5389b912065f8b9f58861c98d3a6dba4311ab5
OnBlock-Dev <47112191+OnBlock@users.noreply.github.com>
2019-10-09 00:47:53 +0330

    Fixed two issues with the BrandedBaharServer

--- src/main/java/com/baharmc/loader/launched/knot/BrandedBaharServer.java
+++ src/main/java/com/baharmc/loader/launched/knot/BrandedBaharServer.java
@@ -4,11 +4,10 @@ import net.minecraft.server.MinecraftServer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(MinecraftServer.class)
-public class BrandedBaharServer {
+public abstract class BrandedBaharServer {
 
     @Inject(
             method = "getServerModName", at = @At(
