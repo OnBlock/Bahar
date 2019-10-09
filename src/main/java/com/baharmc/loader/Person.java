28d8475705fc892904004fd29a66eaf195c5e68c
portlek <hsndmrts_@outlook.com>
2019-10-09 10:59:33 +0300

    Update Person.java

--- src/main/java/com/baharmc/loader/Person.java
+++ src/main/java/com/baharmc/loader/Person.java
@@ -2,10 +2,15 @@ package com.baharmc.loader;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.util.List;
+
 public interface Person {
 
     @NotNull
     String getName();
+
+    @NotNull
+    List<String> getRoles();
 
     @NotNull
     Contact getContact();
