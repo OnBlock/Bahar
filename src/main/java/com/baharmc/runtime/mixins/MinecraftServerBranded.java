2f726b51071aba2377b9b8fbc9b02e07dc8f2f54
portlek <hsndmrts_@outlook.com>
2019-10-10 11:39:56 +0300

    mixin json

---
+++ src/main/java/com/baharmc/runtime/mixins/MinecraftServerBranded.java
@@ -0,0 +1,22 @@
+package com.baharmc.runtime.mixins;
+
+import net.minecraft.server.MinecraftServer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(MinecraftServer.class)
+public abstract class MinecraftServerBranded {
+
+    @Inject(
+            method = "getServerModName", at = @At(
+            target = "Lnet/minecraft/server/MinecraftServer;getServerModName()Ljava/lang/String;",
+            value = "RETURN"
+    ))
+
+    private void Bahar$Brand(CallbackInfoReturnable<String> cir) {
+        cir.setReturnValue("Bahar");
+    }
+
+}
