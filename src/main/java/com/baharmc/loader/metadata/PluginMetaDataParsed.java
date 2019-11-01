c8d088c1513bd83a0110682c2dfaa0e7c3ddd1e9
portlek <hsndmrts_@outlook.com>
2019-11-01 18:56:04 +0300

    Update PluginMetaDataParsed.java

--- src/main/java/com/baharmc/loader/metadata/PluginMetaDataParsed.java
+++ src/main/java/com/baharmc/loader/metadata/PluginMetaDataParsed.java
@@ -3,7 +3,6 @@ package com.baharmc.loader.metadata;
 import com.baharmc.loader.plugin.LoadedPluginMetaData;
 import org.cactoos.Scalar;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 import org.yaml.snakeyaml.Yaml;
 
 import java.io.InputStream;
@@ -23,25 +22,14 @@ public final class PluginMetaDataParsed implements Scalar<LoadedPluginMetaData[]
         final Yaml yaml = new Yaml();
         final Map<String, Object> parsed = yaml.load(stream);
 
-        final String id = get(parsed.get("id"), String.class, "Plugin id must be specified!");
-        final String versionString = get(parsed.get("version"), String.class, "Plugin version must be specified!");
-        final String description = get(parsed.get("description"), String.class);
+        final String id = parsed.getOrDefault("id", "");
+        
+        if (id.isEmpty()) {
+            throw new RuntimeException("Plugin id must be specified!");
+        }
 
 
         return new LoadedPluginMetaData[0];
-    }
-
-    private <T> T get(@Nullable Object object, @NotNull Class<T> type, @NotNull String error) {
-        if ((object == null || !object.getClass().isAssignableFrom(type)) && error.isEmpty()) {
-            throw new RuntimeException(error);
-        }
-
-        //noinspection unchecked
-        return (T)object;
-    }
-
-    private <T> T get(@Nullable Object object, @NotNull Class<T> type) {
-        return get(object, type, "");
     }
 
 }
